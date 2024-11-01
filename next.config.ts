c2ab979f739488c578d8d906488ef4f253d65a07
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-11-01 01:25:55 -0300

    add initial setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname:
+          "arc-anglerfish-arc2-sandbox-sandbox-lanacionar.s3.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,46 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { getArticlesData, getTagsData } from "./lib";
+import TagsList from "./components/TagsList";
+import ArticlesList from "./components/ArticlesList";
+import ArticlesListHeader from "./components/ArticlesListHeader";
+import Sidebar from "./components/Sidebar";
 
-export default function Home() {
+export default async function Home() {
+  const tags = await getTagsData();
+  const articles = await getArticlesData();
+
+  if (!tags || !articles) {
+    return <div>Cargando...</div>;
+  }
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <main className="lay-sidebar">
+      <section className="sidebar__main">
+        <div className="row">
+          <ArticlesListHeader title="Acumulado Grilla" />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="row">
+          <div className="cont_tags com-secondary-tag hlp-marginBottom-20">
+            {tags.map((tag) => (
+              <TagsList key={tag.slug} tag={tag} />
+            ))}
+          </div>
+        </div>
+
+        <div className="row-gap-tablet-2 row-gap-deskxl-3 hlp-degrade">
+          {articles.map((article) => (
+            <ArticlesList key={article._id} article={article} />
+          ))}
+        </div>
+
+        <div className="row">
+          <div className="col-12 hlp-text-center hlp-margintop-40">
+            <button type="button" className="--btn --secondary"></button>
+          </div>
+        </div>
+      </section>
+
+      <Sidebar />
+    </main>
   );
 }
