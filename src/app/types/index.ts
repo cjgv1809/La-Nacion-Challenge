f56a5c5fce1ca7af4ce6594d19b2e0f816b0cf8f
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-11-01 01:28:22 -0300

    add types based on endpoint

---
+++ src/app/types/index.ts
@@ -0,0 +1,49 @@
+export interface Article {
+  _id: string;
+  display_date: Date;
+  headlines: Headlines;
+  promo_items?: PromoItems;
+  subtype: string;
+  taxonomy: Taxonomy;
+  website_url: string;
+}
+
+export interface Headlines {
+  basic: string;
+}
+
+export interface PromoItems {
+  basic: Basic;
+}
+
+export interface Basic {
+  resized_urls?: ResizedURL[];
+  subtitle?: string;
+  type: Type;
+  url?: string;
+}
+
+export interface ResizedURL {
+  option: Option;
+  resizedUrl: string;
+}
+
+export interface Option {
+  media: Media;
+}
+
+export type Media =
+  | "(min-width: 64em)"
+  | "(min-width: 48em)"
+  | "(min-width: 20em)";
+
+export type Type = "image" | "video";
+
+export interface Taxonomy {
+  tags: Tag[];
+}
+
+export interface Tag {
+  slug: string;
+  text: string;
+}
