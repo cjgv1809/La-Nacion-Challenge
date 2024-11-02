03ebcf0697da8e6fa158112874d49f67b77cf460
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-11-02 20:16:24 -0300

    refactor getArticlesData function

--- src/app/lib/getArticlesData.ts
+++ src/app/lib/getArticlesData.ts
@@ -1,6 +1,14 @@
 import type { Article } from "../types";
 
-async function getArticlesData(filterFlag?: boolean): Promise<Article[]> {
+interface getArticlesDataProps {
+  filterFlag?: boolean;
+  searchTerm?: string;
+}
+
+async function getArticlesData({
+  filterFlag = false,
+  searchTerm = "",
+}: getArticlesDataProps = {}): Promise<Article[]> {
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
       cache: "no-store",
@@ -11,19 +19,24 @@ async function getArticlesData(filterFlag?: boolean): Promise<Article[]> {
     }
 
     const { articles } = await response.json();
-    
+
     if (!articles) {
       throw new Error("No articles data found");
     }
-    
-    // Filter articles with subtype 7 and get first 30 if filter flag is true
-    if (filterFlag) {
-      return articles
-      .filter((article: Article) => article.subtype === "7")
-      .slice(0, 30);
+
+    // Filter articles with subtype 7 if filterFlag is true
+    let filteredArticles = filterFlag
+      ? articles.filter((article: Article) => article.subtype === "7")
+      : articles;
+
+    // Filter by searchTerm if provided
+    if (searchTerm) {
+      filteredArticles = filteredArticles.filter((article: Article) =>
+        article.headlines.basic.toLowerCase().includes(searchTerm.toLowerCase())
+      );
     }
-    
-    return articles;
+
+    return filteredArticles;
   } catch (error) {
     console.error("Error fetching articles:", error);
     return [];
