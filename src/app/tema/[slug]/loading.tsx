bacf165b3552706fc9993252f83a98c66ef09692
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-11-02 01:24:29 -0300

    create TagSlug page and its loading file

---
+++ src/app/tema/[slug]/loading.tsx
@@ -0,0 +1,11 @@
+function Loading() {
+  return (
+    <div className="loader">
+      <div className="outer"></div>
+      <div className="middle"></div>
+      <div className="inner"></div>
+    </div>
+  );
+}
+
+export default Loading;
---
+++ src/app/tema/[slug]/page.tsx
@@ -0,0 +1,60 @@
+import ArticlesList from "@/app/components/ArticlesList";
+import Button from "@/app/components/Button";
+import HeaderTitle from "@/app/components/HeaderTitle";
+import Sidebar from "@/app/components/Sidebar";
+import TagsList from "@/app/components/TagsList";
+import { filteredArticlesByTag, getArticlesData, getTagsData } from "@/app/lib";
+
+interface TagPageProps {
+  params: { slug: string };
+}
+
+export default async function TagPage({ params }: TagPageProps) {
+  const { slug } = await params;
+  // execute both promises in parallel
+  const [tagsResult, articlesResult] = await Promise.allSettled([
+    getTagsData(),
+    getArticlesData(),
+  ]);
+
+  if (
+    tagsResult.status !== "fulfilled" ||
+    articlesResult.status !== "fulfilled"
+  ) {
+    return <div>Error al cargar los datos</div>;
+  }
+
+  const tags = tagsResult.value;
+  const articles = articlesResult.value;
+  const filteredArticles = filteredArticlesByTag(articles, slug);
+
+  return (
+    <main className="lay-sidebar">
+      <section className="sidebar__main">
+        <div className="row">
+          <HeaderTitle title="Acumulado Grilla" />
+        </div>
+
+        <div className="row">
+          <div className="cont_tags com-secondary-tag hlp-marginBottom-20">
+            {tags.map((tag) => (
+              <TagsList key={tag.slug} tag={tag} />
+            ))}
+          </div>
+        </div>
+
+        <div className="row-gap-tablet-2 row-gap-deskxl-3 hlp-degrade">
+          {filteredArticles.map((article) => (
+            <ArticlesList key={article._id} article={article} />
+          ))}
+        </div>
+
+        <div className="row">
+          <Button text="MÁS NOTAS DE ACUMULADO GRILLA" />
+        </div>
+      </section>
+
+      <Sidebar />
+    </main>
+  );
+}
