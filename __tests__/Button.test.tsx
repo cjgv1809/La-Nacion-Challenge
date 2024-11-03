753e767b7f6f54c2478113db36118a0510e52995
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-11-02 22:22:01 -0300

    add initial tests

---
+++ __tests__/Button.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+import Button from "@/app/components/Button";
+
+describe("Button", () => {
+  it("renders a button", () => {
+    render(<Button text="Click me" />);
+
+    const button = screen.getByRole("button");
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it("renders the exact text passed to it", () => {
+    render(<Button text="Click me" />);
+
+    const button = screen.getByRole("button");
+
+    expect(button).toHaveTextContent("Click me");
+  });
+
+  it("renders the correct class", () => {
+    render(<Button text="Click me" />);
+
+    const button = screen.getByRole("button");
+
+    expect(button).toHaveClass("--btn --secondary");
+  });
+
+  it("the text passed to it is not empty", () => {
+    render(<Button text="Click me" />);
+
+    const button = screen.getByRole("button");
+
+    expect(button).not.toHaveTextContent("");
+  });
+});
---
+++ __tests__/HeaderTitle.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import HeaderTitle from "@/app/components/HeaderTitle";
+
+describe("HeaderTitle", () => {
+  it("renders a heading", () => {
+    render(<HeaderTitle title="Hello, world" />);
+
+    const heading = screen.getByRole("heading", { level: 1 });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it("renders the exact title passed to it", () => {
+    render(<HeaderTitle title="Hello, world" />);
+
+    const heading = screen.getByRole("heading", { level: 1 });
+
+    expect(heading).toHaveTextContent("Hello, world");
+  });
+});
